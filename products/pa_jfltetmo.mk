0c3768b8b1a501f1a9bae6d82c245122ea1f0d4b
ayysir <dresadd09691@gmail.com>
2013-06-04 01:27:30 -0400

    jfltetmo.mk
    and so it begins

---
+++ products/pa_jfltetmo.mk
@@ -0,0 +1,61 @@
+# Copyright (C) 2012 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Check for target product
+ifeq (pa_jfltetmo,$(TARGET_PRODUCT))
+
+# Define PA bootanimation size
+PARANOID_BOOTANIMATION_NAME := XHDPI
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xhdpi
+
+# Build paprefs from sources
+PREFS_FROM_SOURCE ?= false
+
+# Include ParanoidAndroid common configuration
+include vendor/pa/config/pa_common.mk
+# inherit from common d2
+-include device/samsung/d2-common/BoardConfigCommon.mk
+# inherit from the proprietary version
+-include vendor/samsung/jfltetmo/BoardConfigVendor.mk
+
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from jfltetmo device
+$(call inherit-product, device/samsung/jfltetmo/device.mk)
+$(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
+# CM Package Extras
+-include vendor/pa/packages/cm.mk
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_jfltetmo
+PRODUCT_DEVICE := jfltetmo
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SGH-M919
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltetmo TARGET_DEVICE=d2tmo BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.2.2/JDQ39/M919UVUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jfltetmo-user 4.2.2 JDQ39 M919UVUAMDB release-keys"
+
+# Update local_manifest.xml
+GET_VENDOR_PROPS := $(shell vendor/pa/tools/getvendorprops.py $(PRODUCT_NAME))
+GET_PROJECT_RMS := $(shell vendor/pa/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py $(PRODUCT_NAME))
+GET_CM_PROJECT_ADDS := $(shell vendor/pa/tools/addprojects.py cm.adds)
+
+endif
