409ef34da9e370a1e3e90f26595d3d78d6cb4012
ayysir <dresadd09691@gmail.com>
2013-06-08 00:23:25 -0400

    Revert "Add Gallery2.apk to common"
    This reverts commit b4bafc02879e1cd25ab7593c2c3033f24e8b1698.

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -6,7 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Copy specific ROM files
 PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk
+    vendor/pa/prebuilt/common/apk/GooManager.apk:system/app/GooManager.apk 
 	
 # init.d support
 PRODUCT_COPY_FILES += \
@@ -30,10 +30,7 @@ PRODUCT_COPY_FILES += \
 
 # Extras
 PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/common/bin/fstrim:system/bin/fstrim \
-    vendor/pa/prebuilt/common/apk/Gallery2.apk:system/app/Gallery2.apk \
-    vendor/pa/prebuilt/common/etc/media_profiles.xml:system/etc/media_profiles.xml
-
+    vendor/pa/prebuilt/common/bin/fstrim:system/bin/fstrim
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
