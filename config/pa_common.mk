bd0298a73e38363f623e26b2099faf0309c5d41d
ayysir <dresadd09691@gmail.com>
2013-06-08 23:27:52 -0400

    re-implement gallery apk

--- config/pa_common.mk
+++ config/pa_common.mk
@@ -30,7 +30,9 @@ PRODUCT_COPY_FILES += \
 
 # Extras
 PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/common/bin/fstrim:system/bin/fstrim
+    vendor/pa/prebuilt/common/bin/fstrim:system/bin/fstrim \
+    vendor/pa/prebuilt/common/apk/Gallery2.apk:system/app/Gallery2.apk \
+    vendor/pa/prebuilt/common/etc/media_profiles.xml:system/etc/media_profiles.xml
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
