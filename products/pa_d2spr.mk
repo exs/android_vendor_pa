8a0d6ebfb69f67face4a75bb9840af9b8b532088
ayysir <dresadd09691@gmail.com>
2013-06-16 19:06:29 -0400

    Hopefully fix LTE issues

--- products/pa_d2spr.mk
+++ products/pa_d2spr.mk
@@ -40,7 +40,7 @@ $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 # CM Package Extras
 -include vendor/pa/packages/cm.mk
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/pa/config/cdma.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
