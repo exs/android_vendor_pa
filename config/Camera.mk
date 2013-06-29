fc56083939fddf17f175a1b1d762a58c3d4a4a90
ayysir <dresadd09691@gmail.com>
2013-06-28 23:45:51 -0400

    CM 10.1 Gallery

---
+++ config/Camera.mk
@@ -0,0 +1,4 @@
+# Google Edition Camera
+PRODUCT_COPY_FILES += \
+    vendor/pa/prebuilt/common/apk/Gallery2.apk:system/app/Gallery2.apk
+
--- config/GECamera.mk
+++
@@ -1,4 +0,0 @@
-# Google Edition Camera
-PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/common/apk/Googleeditioncamera.apk:system/app/Gallery2.apk
-
